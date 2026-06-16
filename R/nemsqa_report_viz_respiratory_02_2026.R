2932c3c75f5b75b0ef682232694b2e7d2980a4a3
bemts-hhs <nicolas.foss@hhs.iowa.gov>
2026-06-16 11:55:08 -0500

    complete visualizations for respiratory_02

--- R/nemsqa_report_viz_respiratory_02_2026.R
+++ R/nemsqa_report_viz_respiratory_02_2026.R
@@ -1,14 +1,14 @@
-### IOWA NEMSQA REPORT VISUALIZATIONS Respiratory-02 2025 ---------------------------
+### IOWA NEMSQA REPORT VISUALIZATIONS Respiratory-02 2026 ---------------------------
 
 ###_____________________________________________________________________________
 # this script will contain all reporting visualizations for Respiratory-02 use:
-# nemsqa_report_prep_2025.R to get critical functions into memory
-# nemsqa_report_respiratory_02_2025.R to generate statistical files for the report
+# nemsqa_report_prep_2026.R to get critical functions into memory
+# nemsqa_report_respiratory_02_2026.R to generate statistical files for the report
 ###_____________________________________________________________________________
 # assume:
-# that nemsqa_report_prep_2025.R was already ran to load needed packages
+# that nemsqa_report_prep_2026.R was already ran to load needed packages
 # and project-specific custom functions in the project
-# nemsqa_report_respiratory_02_2025.R was ran to generate statistical files
+# nemsqa_report_respiratory_02_2026.R was ran to generate statistical files
 ###_____________________________________________________________________________
 
 ### DATA -----------------------------------------------------------------------
@@ -16,11 +16,11 @@
 # import statistical outputs for this measure
 import_nemsqa_statistical_files(measure = "Respiratory-02")
 
-### TABLES ---------------------------------------------------------------------
+# TABLES ---------------------------------------------------------------------
 
-### population data ############################################################
+# population data ############################################################
 
-# generate the population gt table
+## generate the population gt table ----
 respiratory_02_pop_gt <- respiratory_02_pop_years |>
   prepare_population_statistical_file() |>
   population_statistical_file_gt(
@@ -48,9 +48,9 @@ export_nemsqa_gt(
   extension = "png"
 )
 
-### results data ###############################################################
+# results data ###############################################################
 
-# generate the results gt table
+## generate the results gt table ----
 respiratory_02_results_gt <- respiratory_02_result_year |>
   prepare_results_statistical_file() |>
   results_statistical_file_gt(groups = c("INCIDENT_YEAR")) |>
@@ -86,14 +86,14 @@ export_nemsqa_gt(
 # Run only once per session
 iowa_counties_sf <- prepare_map_data()
 
-# summarize performance statewide over the timeframe of interest
+## summarize performance statewide over the timeframe of interest ----
 respiratory_02_result_counties_map <- results_to_county_map(
   df = respiratory_02_result_counties,
   add_text = FALSE,
   format = "percent"
 )
 
-# save the plot
+## save the plot ----
 ggplot2::ggsave(
   filename = "respiratory_02_result_counties_map.png",
   plot = respiratory_02_result_counties_map,
